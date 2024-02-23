25cb910c748075f03b694b340b2b6c3b862d84c3
Micael Gomes <contato.micaeloficial@gmail.com>
2024-02-23 03:00:02 -0300

    lint: new lint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-
-
 function App() {
 
 
   return (
     <>
-      <div className="w-100 h-96 flex justify-center items-center	align-center ">
+      <div className="w-100 h-96 flex justify-center items-center align-center">
         <h1>Hello world!</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
