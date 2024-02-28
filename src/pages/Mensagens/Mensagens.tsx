ecdf995918b63c5193ceaae3565b60e567d90c78
Micael Gomes <contato.micaeloficial@gmail.com>
2024-02-28 01:39:37 -0300

    Feat: Mensagens page and data mock

---
+++ src/pages/Mensagens/Mensagens.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { mensagens } from './mensagens-mock';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+
+const MensagemCard = ({ mensagem }: any) => {
+  return (
+    <>
+      {mensagem.mensagens.map((msg: any, index: any) => (
+        <Card key={index} className="w-[350px]">
+          <CardHeader>
+            <CardTitle className='mb-[3px] flex space-between'>
+              {mensagem.professor}
+            </CardTitle>
+            <span className="text-sm">{msg.data}</span>
+          </CardHeader>
+          <CardContent className="flex flex-col">
+            {msg.mensagem}
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+const CursoFilter = ({ cursoSelecionado, setCursoSelecionado }: any) => {
+  const disciplinas = [...new Set(mensagens.map(msg => msg.disciplina))];
+
+  const handleChangeCurso = (event: any) => {
+    setCursoSelecionado(event.target.value);
+  };
+
+  return (
+    <select className="mt-3" value={cursoSelecionado} onChange={handleChangeCurso}>
+      <option value="">Todas as disciplinas</option>
+      {disciplinas.map(disciplina => (
+        <option key={disciplina} value={disciplina}>
+          {disciplina}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const getMensagensFiltradas = (mensagens: any, cursoSelecionado: any) => {
+  if (!cursoSelecionado) {
+    return mensagens;
+  }
+  return mensagens.filter((msg: { disciplina: any; }) => msg.disciplina === cursoSelecionado);
+};
+
+const Mensagens = () => {
+  const [cursoSelecionado, setCursoSelecionado] = useState('');
+  const mensagensFiltradas = getMensagensFiltradas(mensagens, cursoSelecionado);
+
+  return (
+    <main className="flex justify-center align-center">
+      <section className="my-24">
+        <div className="flex justify-between">
+          <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl'>Mensagens das Disciplinas</h1>
+          <a href="/"><Button>Voltar</Button></a>
+        </div>
+
+        <CursoFilter cursoSelecionado={cursoSelecionado} setCursoSelecionado={setCursoSelecionado} />
+
+        <div className="grid grid-cols-4 gap-4 my-6">
+          {mensagensFiltradas.map((mensagem: any, index: any) => (
+            <MensagemCard key={index} mensagem={mensagem} />
+          ))}
+        </div>
+
+      </section>
+    </main>
+  );
+};
+
+export default Mensagens;
---
+++ src/pages/Mensagens/mensagens-mock.ts
@@ -0,0 +1,30 @@
+export const mensagens = [
+  {
+    id: 1,
+    disciplina: 'Programação I',
+    professor: 'Clara',
+    mensagens: [
+      {
+        data: '10/02/2024',
+        mensagem: 'Bom dia a todos, passando só para avisar que as notas da P1 estão lançadas no sistema acadêmico. Lancei as notas das atividades feitas em aula já somadas e a nota da avaliação separada. Abraço!'
+      }
+    ]
+  },
+  {
+    id: 2,
+    disciplina: 'Gerência de Projetos',
+    professor: 'Fernando Carvalho',
+    mensagens: [
+      {
+        data: '10/02/2024',
+        mensagem: 'Boa tarde, devido a um problema de energia no campus a aula está cancelada no dia de hoje.'
+      },
+      {
+        data: '28/02/2024',
+        mensagem: 'Trabalho hoje'
+      }
+    ]
+  }
+];
+
+export default mensagens;
