4d5264f8f9e2de13032aa1e4493fdf61227651d5
Micael Gomes <contato.micaeloficial@gmail.com>
2024-02-28 13:38:57 -0300

    Docs: removing unused imports

--- src/pages/cronograma/Disciplinas.tsx
+++ src/pages/cronograma/Disciplinas.tsx
@@ -11,7 +11,6 @@ import {
 
 import { alunosData } from "../../mocks/aluno.ts";
 import { Link } from "react-router-dom";
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Key } from "react";
 
 const Disciplinas = () => {
 
