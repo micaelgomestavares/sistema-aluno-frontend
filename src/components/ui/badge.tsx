c4ed6911675b43916631f803a516d5bcb31fb9b3
Micael Gomes <contato.micaeloficial@gmail.com>
2024-02-28 13:38:29 -0300

    Feat: adding new variants to button and badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,6 +14,8 @@ const badgeVariants = cva(
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         concluded:
           "border-transparent bg-green-700 text-white hover:bg-green-900",
+        trabalho:
+          "border-transparent bg-violet-700	text-white hover:bg-violet-900",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        success: "bg-green-700 text-white hover:bg-green-900/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -35,7 +36,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
