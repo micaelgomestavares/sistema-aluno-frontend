b2fd350a038ee688e2c17adeac283ec59165e19a
Micael Gomes <contato.micaeloficial@gmail.com>
2024-03-03 21:25:43 -0300

    Style: fixing some style paddins and margins

--- src/pages/SubmeterTrabalho_DetalharProva/DetalharProva.tsx
+++ src/pages/SubmeterTrabalho_DetalharProva/DetalharProva.tsx
@@ -9,58 +9,63 @@ import {
 import { provas } from 'src/pages/SubmeterTrabalho_DetalharProva/Provas.ts';
 
 function DetalharProva() {
-    const provaId = 1; // FAZER A LIGAÇÃO COM A PAGINA DE CONSULTA DE PROVAS
-    const prova = provas.find(p => p.id === provaId);
-  
+  const provaId = 2; // FAZER A LIGAÇÃO COM A PAGINA DE CONSULTA DE PROVAS
+  const prova = provas.find(p => p.id === provaId);
+
   return (
-    <div>
-      {prova && (
-        <Card>
-          <CardHeader className="flex items-left">
-            <div className="flex justify-between mb-10">
-              <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl'>Detalhes da Prova</h1>
-              <a href="/"><Button>Voltar</Button></a>
-            </div>
-            <div className="w-full">
-            <CardTitle className="text-center mb-5 text-4xl font-bold leading-relaxed">{prova.titulo}</CardTitle>
-            </div>
-            <div className="mt-1">
-              <Badge variant="secondary" className="bg-blue-500 text-lg text-white cursor-default hover:bg-blue-700">{prova.disciplina}</Badge>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <div><Badge variant="secondary" className={'text-md mb-6 bg-gray-200 text-gray-800' }>
-              Data: {prova.data.toLocaleString('pt-BR')}
-            </Badge>
-            </div>
-            <div><Badge variant="secondary" className={'text-md mb-10 bg-gray-200 text-gray-800' }>
-              Local: {prova.sala}
-            </Badge>
-            </div>
-            <div className="w-1/2 bg-gray-100">
-              <div >
-                <CardTitle className="text-center mb-8">Conteúdo :</CardTitle>
-              </div>
-              <div >
-                <ol className="text-md list-decimal pl-5 w-full">
-                {prova.conteudo.map(conteudo => (
-                  <li className='mb-6'>
-                    {conteudo.topico} :
-                    <ul className={'text-md list-disc pl-5' }>
-                      {conteudo.assuntos.split('/').map((item) => (
-                        <li >{item}</li>
+    <main className="flex justify-center align-center">
+      <section className="my-24">
+        <div>
+          {prova && (
+            <Card>
+              <CardHeader className="flex items-left">
+                <div className="flex justify-between mb-10">
+                  <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl'>Detalhes da Prova</h1>
+                  <a href="/"><Button>Voltar</Button></a>
+                </div>
+                <div className="w-full">
+                  <CardTitle className="text-center mb-5 text-4xl font-bold leading-relaxed">{prova.titulo}</CardTitle>
+                </div>
+                <div className="mt-1">
+                  <Badge variant="secondary" className="bg-blue-500 text-lg text-white cursor-default hover:bg-blue-700">{prova.disciplina}</Badge>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <div><Badge variant="secondary" className={'text-md mb-6 bg-gray-200 text-gray-800'}>
+                  Data: {prova.data.toLocaleString('pt-BR')}
+                </Badge>
+                </div>
+                <div><Badge variant="secondary" className={'text-md mb-10 bg-gray-200 text-gray-800'}>
+                  Local: {prova.sala}
+                </Badge>
+                </div>
+                <div className="bg-gray-100 p-4">
+                  <div >
+                    <CardTitle className="text-center mb-8">Conteúdo:</CardTitle>
+                  </div>
+                  <div >
+                    <ol className="text-md list-decimal pl-5 w-full">
+                      {prova.conteudo.map(conteudo => (
+                        <li className='mb-6'>
+                          {conteudo.topico} :
+                          <ul className={'text-md list-disc pl-5'}>
+                            {conteudo.assuntos.split('/').map((item) => (
+                              <li >{item}</li>
+                            ))}
+                          </ul>
+                        </li>
                       ))}
-                    </ul>
-                  </li>
-                ))}
-                </ol>
-              </div>
-            </div>
-          </CardContent >
-        </Card>
-      )}
-    </div>
+                    </ol>
+                  </div>
+                </div>
+              </CardContent >
+            </Card>
+          )}
+        </div>
+      </section>
+    </main>
   );
+
 }
 
 export default DetalharProva;
--- src/pages/SubmeterTrabalho_DetalharProva/SubmeterTrabalho.tsx
+++ src/pages/SubmeterTrabalho_DetalharProva/SubmeterTrabalho.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from "@/components/ui/input";
 import classNames from 'classnames';
 import { format } from "date-fns";
@@ -13,7 +13,7 @@ function SubmeterTrabalho() {
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
 
   const [trabalhosState, setTrabalhosState] = useState(trabalhos);
-  const trabalhoId = 1; // FAZER A LIGAÇÃO COM A PAGINA DE CONSULTA DE PROVAS
+  const trabalhoId = 2; // FAZER A LIGAÇÃO COM A PAGINA DE CONSULTA DE PROVAS
   const trabalho = trabalhosState.find((t) => t.id === trabalhoId);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,53 +41,57 @@ function SubmeterTrabalho() {
   const badgeClasses = classNames(
     "text-sm mb-10 bg-gray-200",
     {
-      "text-red-500": format(trabalho?.dataEntrega, "yyyy-MM-dd") < format(new Date(), "yyyy-MM-dd"),
+      "text-red-500": format(trabalho?.dataEntrega ?? "", "yyyy-MM-dd") < format(new Date(), "yyyy-MM-dd"),
       "text-green-500": trabalho?.situacao === "enviado",
       "text-gray-800":
         trabalho?.situacao !== "enviado" &&
-        format(trabalho?.dataEntrega, "yyyy-MM-dd") >= format(new Date(), "yyyy-MM-dd"),
+        format(trabalho?.dataEntrega ?? "", "yyyy-MM-dd") >= format(new Date(), "yyyy-MM-dd"),
     }
   );
 
   return (
-    <div>
-      {trabalho && (
-        <Card>
-          <CardHeader className="flex items-left">
-            <div className="flex justify-between mb-10">
-              <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl'>Submeter Trabalho</h1>
-              <a href="/"><Button>Voltar</Button></a>
-            </div>
-            <div className="w-full">
-              <CardTitle className="text-center mb-5 text-4xl font-bold leading-relaxed">{trabalho.titulo}</CardTitle>
-            </div>
-            <div className="mt-1">
-              <Badge variant="secondary" className="text-lg">{trabalho.disciplina}</Badge>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <Badge variant="secondary" className={badgeClasses}>
-              Data de Entrega: {trabalho.dataEntrega.toLocaleString('pt-BR')}
-            </Badge>
-            <p>{trabalho.descricao}</p>
-          </CardContent >
-          <form onSubmit={handleSubmit}>
-            <Input type="file" className="mb-10" onChange={handleFileChange} />
-            <div className="flex justify-center">
-              {trabalho.situacao !== 'enviado' && format(trabalho.dataEntrega,"yyyy-MM-dd") >= format(new Date(), "yyyy-MM-dd") ? (
-                <Button type="submit" disabled={submitButtonDisabled}>
-                  {submitButtonDisabled ? submissionMessage : "Enviar Trabalho"}
-                </Button>
-              ) : trabalho.situacao !== 'enviado' && format(trabalho.dataEntrega,"yyyy-MM-dd") < format(new Date(), "yyyy-MM-dd") ? (
-                <p key="atrasado" className="text-red-500">Trabalho atrasado!</p>
-              ) : (
-                <p key="enviado" className="text-green-500">{`O trabalho foi enviado no dia ${format(trabalho.dataEnvio, "dd/MM/yyyy")}`}</p>
-              )}
-            </div>
-          </form>
-        </Card>
-      )}
-    </div>
+    <main className="flex justify-center align-center">
+      <section className="my-24">
+        <div>
+          {trabalho && (
+            <Card className="p-4">
+              <CardHeader className="flex items-left">
+                <div className="flex justify-between mb-10">
+                  <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl'>Submeter Trabalho</h1>
+                  <a href="/"><Button>Voltar</Button></a>
+                </div>
+                <div className="w-full">
+                  <CardTitle className="text-center mb-5 text-4xl font-bold leading-relaxed">{trabalho.titulo}</CardTitle>
+                </div>
+                <div className="mt-1">
+                  <Badge variant="secondary" className="text-lg">{trabalho.disciplina}</Badge>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Badge variant="secondary" className={badgeClasses}>
+                  Data de Entrega: {trabalho.dataEntrega.toLocaleString('pt-BR')}
+                </Badge>
+                <p>{trabalho.descricao}</p>
+              </CardContent >
+              <form onSubmit={handleSubmit}>
+                <Input type="file" className="mb-10" onChange={handleFileChange} />
+                <div className="flex justify-center">
+                  {trabalho.situacao !== 'enviado' && format(trabalho.dataEntrega, "yyyy-MM-dd") >= format(new Date(), "yyyy-MM-dd") ? (
+                    <Button type="submit" disabled={submitButtonDisabled}>
+                      {submitButtonDisabled ? submissionMessage : "Enviar Trabalho"}
+                    </Button>
+                  ) : trabalho.situacao !== 'enviado' && format(trabalho.dataEntrega, "yyyy-MM-dd") < format(new Date(), "yyyy-MM-dd") ? (
+                    <p key="atrasado" className="text-red-500">Trabalho atrasado!</p>
+                  ) : (
+                    <p key="enviado" className="text-green-500">{`O trabalho foi enviado no dia ${format(trabalho.dataEnvio ?? "", "dd/MM/yyyy")}`}</p>
+                  )}
+                </div>
+              </form>
+            </Card>
+          )}
+        </div>
+      </section>
+    </main>
   );
 }
 
