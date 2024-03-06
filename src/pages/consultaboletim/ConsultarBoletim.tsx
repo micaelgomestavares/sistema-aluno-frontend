53faceec0ec40626a72445da8c9c67d8d01a3d4d
Micael Gomes <contato.micaeloficial@gmail.com>
2024-03-06 01:49:28 -0300

    Fix: adding table and fixing types

--- src/pages/consultaboletim/ConsultarBoletim.tsx
+++ src/pages/consultaboletim/ConsultarBoletim.tsx
@@ -1,66 +1,80 @@
-import React, { useState } from 'react'
-
+import { useState } from 'react'
 import { boletim } from '@/mocks/boletim'
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Button } from '@/components/ui/button';
+
 
 const ConsultarBoletim = () => {
 
     const [sem, setSem] = useState("2023/1");
 
-    const changeSem = e => {
+    const changeSem = (e: any) => {
         setSem(e.target.value);
     }
-
     const dados = boletim;
-
     const semestres = Object.keys(dados);
-    console.log(semestres);
-    return (
-        <>
-            <select name="semestres" id="semestre" onChange={changeSem}>
-                {semestres.map((semestre,index) => (
-                    <option value={semestre} key={semestre}>{semestre}</option>    
-                ))}
-            </select>
-            <table className="min-w-full border">
-                <thead>
-                    <tr>
-                        <th className="border"> Componente Currícular</th>
-                        <th className="border"> CH</th>
-                        <th className="border"> Turma</th>
-                        <th className="border"> T.Faltas</th>
-                        <th className="border"> M.Final</th>
-                        <th className="border"> A1</th>
-                        <th className="border"> F</th>
-                        <th className="border"> A2</th>
-                        <th className="border"> F</th>
-                        <th className="border"> A3</th>
-                        <th className="border"> F</th>
-                        <th className="border"> MF</th>
-                        <th className="border"> Situação</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dados[sem].map((disciplina) => (
-                        <tr key={disciplina.id} className={disciplina.situacao === 'Aprovado' ? 'bg-green-100' : 'bg-red-100'}>
-                            <td className="border">{disciplina.nome}</td>
-                            <td className="border">{disciplina.ch}</td>
-                            <td className="border">{disciplina.turma}</td>
-                            <td className="border">{disciplina.total_faltas}</td>
-                            <td className="border">{disciplina.media_final}</td>
-                            <td className="border">{disciplina.a1}</td>
-                            <td className="border">{disciplina.f1}</td>
-                            <td className="border">{disciplina.a2}</td>
-                            <td className="border">{disciplina.f2}</td>
-                            <td className="border">{disciplina.a3}</td>
-                            <td className="border">{disciplina.f3}</td>
-                            <td className="border">{disciplina.mf}</td>
-                            <td className="border">{disciplina.situacao}</td>
-                        </tr>
-                    ))}
-                </tbody>
 
-            </table>
-        </>
+    return (
+        <main className="flex justify-center align-center">
+            <section className="my-24">
+
+                <div className='flex justify-between'>
+                    <select className='p-2' name="semestres" id="semestre" onChange={changeSem}>
+                        {semestres.map((semestre) => (
+                            <option value={semestre} key={semestre}>{semestre}</option>
+                        ))}
+                    </select>
+                    <a href="/"><Button>Voltar</Button></a>
+                </div>
+                <Table className="min-w-full border mt-4">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="border"> Componente Currícular</TableHead>
+                            <TableHead className="border"> CH</TableHead>
+                            <TableHead className="border"> Turma</TableHead>
+                            <TableHead className="border"> T.Faltas</TableHead>
+                            <TableHead className="border"> M.Final</TableHead>
+                            <TableHead className="border"> A1</TableHead>
+                            <TableHead className="border"> F</TableHead>
+                            <TableHead className="border"> A2</TableHead>
+                            <TableHead className="border"> F</TableHead>
+                            <TableHead className="border"> A3</TableHead>
+                            <TableHead className="border"> F</TableHead>
+                            <TableHead className="border"> MF</TableHead>
+                            <TableHead className="border"> Status</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {dados[sem as keyof typeof dados].map((disciplina: any) => (
+                            <TableRow key={disciplina.id} className={disciplina.situacao === 'Aprovado' ? 'bg-green-100' : 'bg-red-100'}>
+                                <TableCell className="border">{disciplina.nome}</TableCell >
+                                <TableCell className="border">{disciplina.ch}</TableCell >
+                                <TableCell className="border">{disciplina.turma}</TableCell >
+                                <TableCell className="border">{disciplina.total_faltas}</TableCell >
+                                <TableCell className="border">{disciplina.media_final}</TableCell >
+                                <TableCell className="border">{disciplina.a1}</TableCell >
+                                <TableCell className="border">{disciplina.f1}</TableCell >
+                                <TableCell className="border">{disciplina.a2}</TableCell >
+                                <TableCell className="border">{disciplina.f2}</TableCell >
+                                <TableCell className="border">{disciplina.a3}</TableCell >
+                                <TableCell className="border">{disciplina.f3}</TableCell >
+                                <TableCell className="border">{disciplina.mf}</TableCell >
+                                <TableCell className="border">{disciplina.situacao}</TableCell >
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </section>
+        </main>
     )
 }
 
